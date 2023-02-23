370b46ffb8c611ec3f8ce4f8920bf7f85da92894
Poliane F De Godoy <122944387+pollygodoy@users.noreply.github.com>
2023-02-23 18:05:06 -0300

    Add files via upload

---
+++ carro.java
@@ -0,0 +1,28 @@
+* Carro
+public class Carro {
+   
+    public static void main(String [] args) {
+        System.out.println("A marca do carro");
+        modelo();
+    }
+    public static void modelo() {
+        System.out.println("Celta");
+        System.out.println("Possui uma velocidade de:");
+        try {
+            velocidade();
+        } 
+        System.out.println("Ano de fabricação de:");
+        ano de fabricação();
+
+    }
+
+    public static void velocidade() {
+        int vel = 10;
+        System.out.println(vel + "km/h");
+    }
+    public static void aceleracao() {
+        int vel = 10;
+        System.out.println(acel + "km/h");
+    }
+
+}
